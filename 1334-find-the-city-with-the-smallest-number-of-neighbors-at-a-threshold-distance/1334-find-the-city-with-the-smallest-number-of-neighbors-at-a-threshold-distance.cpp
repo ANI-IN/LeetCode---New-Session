9a5d9efc869638e3ff017dbf7da945e6f12783f8
Animesh Kumar <44803072+ANI-IN@users.noreply.github.com>
2024-07-26 23:18:31 +0530

    Time: 36 ms (23.68%), Space: 16.1 MB (23.89%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int t) {
-        vector<vector<int>> dis(n,vector<int>(n,1e9));
+        
+        vector<vector<int>> dist(n,vector<int>(n,1e9));
         
         for(int i=0;i<n;i++)
-            dis[i][i]=0;
-        
-         for(auto it : edges)
         {
-            dis[it[0]][it[1]]=it[2];
-            dis[it[1]][it[0]]=it[2];
+            dist[i][i]=0;
+        }
+        
+        for(auto it : edges)
+        {
+            int i=it[0];
+            int j=it[1];
+            int w=it[2];
+            dist[i][j]=w;
+            dist[j][i]=w;
         }
         
         for(int k=0;k<n;k++)
@@ -17,34 +23,40 @@ public:
             for(int i=0;i<n;i++)
             {
                 for(int j=0;j<n;j++)
-                {
-                    if(dis[i][k]==1e9 || dis[k][j]==1e9)
+                {   
+                     if(dist[i][k]==1e9 || dist[k][j]==1e9)
                         continue;
-                    if(dis[i][k]+dis[k][j]<dis[i][j])
-                    {
-                      dis[i][j] = dis[i][k]+dis[k][j];
-                    }
+                     dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
                 }
             }
         }
         
-        int maxi=n;
+        for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                   cout<<dist[i][j]<<" ";
+                }
+                cout<<endl;
+            }
         
+        int maxi=n;
         int city=0;
         for(int i=0;i<n;i++)
         {
             int c=0;
             for(int j=0;j<n;j++)
             {
-                if(dis[i][j]<=t)
+                if(dist[i][j]<=t)
                     c++;
             }
             if(maxi>=c)
             {
                 maxi=c;
-                city=i;
+                city=i;    
             }
         }
         return city;
+        
     }
 };
