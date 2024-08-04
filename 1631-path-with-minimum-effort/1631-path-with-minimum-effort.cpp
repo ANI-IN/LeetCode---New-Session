9e92c1ba64a11ac9ec029d0105ceec09a6e6dc00
Animesh Kumar <44803072+ANI-IN@users.noreply.github.com>
2024-08-05 00:32:17 +0530

    Time: 92 ms (51.53%), Space: 24 MB (37.96%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
-    int minimumEffortPath(vector<vector<int>>& heights) {
-        int ans=0;
-        int n=heights.size();
-        int m=heights[0].size();
+    int minimumEffortPath(vector<vector<int>>& arr) {
+        int n=arr.size();
+        int m=arr[0].size();
+        vector<vector<int>> dist(n,vector<int>(m,1e9));
+        dist[0][0]=0;
         
-        vector<vector<int>> grid(n,vector<int>(m,1e9));
-        
-        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>q;
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>q;
         q.push({0,{0,0}});
         
         int delrow[]={-1,0,+1,0};
@@ -15,33 +14,28 @@ public:
         
         while(!q.empty())
         {
-            auto it=q.top();
+            int d=q.top().first;
+            int r=q.top().second.first;
+            int c=q.top().second.second;
             q.pop();
-            
-            int r=it.second.first;
-            int c=it.second.second;
-            int d=it.first;
-            
-            if(r==n-1 && c==m-1)
-                return d;
             
             for(int i=0;i<4;i++)
             {
                 int nr=r+delrow[i];
                 int nc=c+delcol[i];
                 
-                if(nr>=0 && nr<n && nc>=0 && nc<m)
-                {
-                    int maxi=max( abs(heights[r][c]-heights[nr][nc]) ,d );
-                    
-                    if(maxi<grid[nr][nc])
-                    {
-                        grid[nr][nc]=maxi;
-                        q.push({maxi,{nr,nc}});
+     
+                if(nr>=0 && nr<n && nc>=0 && nc<m )
+                {   
+                    int ef = abs(arr[r][c] - arr[nr][nc]);
+                    int effort=max(ef,d);
+                    if (effort < dist[nr][nc]) {
+                        dist[nr][nc] = effort;
+                        q.push({effort, {nr, nc}});
                     }
                 }
             }
         }
-        return -1;
+        return dist[n-1][m-1];
     }
 };
