7c906a56cef323059e52e9be044d720c5eab7671
Animesh Kumar <44803072+ANI-IN@users.noreply.github.com>
2024-08-04 22:28:57 +0530

    Time: 72 ms (23.17%), Space: 23.3 MB (80.42%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -2,23 +2,26 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
-        int m=grid[0].size();
         if(grid[0][0]==1)
             return -1;
-        int delrow[] = {-1, -1, 0, 1, 1, 1, 0, -1};
-        int delcol[] = {0, 1, 1, 1, 0, -1, -1, -1};
-        queue<pair<int,pair<int,int>>>q;
-        q.push({1,{0,0}}); 
-        vector<vector<int>> dis(n,vector<int>(m,1e9));
-         dis[0][0]=1;
+        vector<vector<int>> dist(n,vector<int>(n,1e9));
+        priority_queue< pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>q;
+        q.push({1,{0,0}});
+        dist[0][0]=1;
+        
+        int delrow[]={-1,-1,0,+1,+1,+1,0,-1};
+        int delcol[]={0,+1,+1,+1,0,-1,-1,-1};
         
         while(!q.empty())
         {
-            int d=q.front().first;
-            int r=q.front().second.first;
-            int c=q.front().second.second;
+            auto t=q.top();
             q.pop();
-            if(r==n-1 && c==m-1)
+            
+            int d=t.first;
+            int r=t.second.first;
+            int c=t.second.second;
+            
+            if(r==n-1 && c==n-1)
                 return d;
             
             for(int i=0;i<8;i++)
@@ -26,14 +29,14 @@ public:
                 int nr=r+delrow[i];
                 int nc=c+delcol[i];
                 
-                if(nr>=0 && nr<n && nc>=0 && nc<m && d+1<dis[nr][nc] && grid[nr][nc]==0)
+                if(nr>=0 && nr<n && nc>=0 && nc<n && grid[nr][nc]==0 && 1+d < dist[nr][nc])
                 {
-                    q.push({d+1,{nr,nc}});
-                    dis[nr][nc]=d+1;
+                    dist[nr][nc]=1+d;
+                    q.push({dist[nr][nc],{nr,nc}});
                 }
             }
         }
         return -1;
-
+        
     }
 };
