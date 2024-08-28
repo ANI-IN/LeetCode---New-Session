28f31a855b7d21a3a498dce4573f92b663bd9e40
Animesh Kumar <44803072+ANI-IN@users.noreply.github.com>
2024-08-28 16:33:46 +0530

    Time: 285 ms (35.02%), Space: 228.5 MB (9.36%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    bool solve(int i, int j, vector<vector<int>>& grid1, vector<vector<int>>& grid2, vector<vector<int>>& vis)
+    int solve(int i, int j, vector<vector<int>>& grid1, vector<vector<int>>& grid2, vector<vector<int>>& vis)
     {
         queue<pair<int,int>> q;
         int n=grid1.size();
@@ -13,43 +13,38 @@ class Solution {
         
         bool island=true;
         
-        if(grid1[i][j]!=1)
-            island=false;
-        
         while(!q.empty())
         {
             int r=q.front().first;
             int c=q.front().second;
             q.pop();
             
-            
+            if(grid1[r][c]!=1)
+            island=false;
             
             for(int k=0;k<4;k++)
             {
                 int nr=r+delrow[k];
                 int nc=c+delcol[k];
                 
-                if(nr>=0 && nr<n && nc>=0 && nc<m)
+                if(nr>=0 && nr<n && nc>=0 && nc<m && grid2[nr][nc]==1 && !vis[nr][nc])
                 {
-                    if(grid2[nr][nc]==1 && !vis[nr][nc])
-                    {
                         vis[nr][nc]=1;
                         q.push({nr,nc});
-                        
-                        if(grid1[nr][nc]!=1)
-                          island=false;  
-                    }
                 }
             }
         }
         
-        return island;
+        if(island)
+            return 1;
+        else
+            return 0;
     }
 public:
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
         int n=grid1.size();
         int m=grid2[0].size();
-        int ans=0;
+        int c=0;
         vector<vector<int>> vis(n,vector<int>(m,0));
         
         for(int i=0;i<n;i++)
@@ -58,11 +53,11 @@ public:
             {
                 if(grid2[i][j]==1 && !vis[i][j])
                 {
-                 if(solve(i,j,grid1,grid2,vis))
-                     ans++;
+                    int ans=solve(i,j,grid1,grid2,vis);
+                    c=c+ans;
                 }
             }
         }
-        return ans;
+        return c;
     }
 };
