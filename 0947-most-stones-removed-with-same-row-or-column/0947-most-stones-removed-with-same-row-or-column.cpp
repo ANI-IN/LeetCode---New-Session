f45ba892790ab856b11b10354db73917d3fd253c
Animesh Kumar <44803072+ANI-IN@users.noreply.github.com>
2024-08-30 01:47:53 +0530

    Time: 120 ms (15.26%), Space: 18.1 MB (81.75%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,28 +1,31 @@
 class Solution {
-    int dfs(int index, vector<int>&vis,vector<vector<int>>&stones)
+    int solve(int index, vector<int>&vis, vector<vector<int>>& stones)
     {
         vis[index]=1;
         int res=0;
         for(int i=0;i<stones.size();i++)
-        {
-            if(!vis[i] && (stones[index][0] == stones[i][0] || stones[index][1]==stones[i][1]))
-            {
-                res=res+dfs(i,vis,stones)+1;
-            }
+        {   
+            int row=stones[i][0];
+            int col=stones[i][1];
+            
+            if(!vis[i] && (stones[index][0]==row || stones[index][1] == col) )
+               {
+                   res=res+solve(i,vis,stones)+1;
+               }
         }
         return res;
+        
     }
 public:
     int removeStones(vector<vector<int>>& stones) {
         int n=stones.size();
         vector<int> vis(n,0);
-        
         int ans=0;
         for(int i=0;i<n;i++)
         {
             if(!vis[i])
             {
-                ans+=dfs(i,vis,stones);
+                ans+=solve(i,vis,stones);
             }
         }
         return ans;
