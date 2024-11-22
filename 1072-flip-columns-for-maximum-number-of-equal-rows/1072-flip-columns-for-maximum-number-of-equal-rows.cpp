9c7c4ab63ce6cd8db504c5ef3bd73d2c92bedcb6
Animesh Kumar <44803072+ANI-IN@users.noreply.github.com>
2024-11-22 22:30:44 +0530

    Time: 46 ms (43.48%), Space: 75.4 MB (59.70%) - LeetHub

--- 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.cpp
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
-        int n = matrix[0].size();
-        int maxRows = 0;
-
-        for (auto& currRow : matrix) {
-
-            vector<int> inverted(n);
-            int count = 0;
-
-            
-            for (int col = 0; col < n; col++) {
-                inverted[col] = currRow[col] == 0 ? 1 : 0;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        unordered_map<string,int> mp;
+        
+        
+        for(int i=0;i<n;i++)
+        {   
+            int curr=matrix[i][0];
+            string check="";
+            for(int j=0;j<m;j++)
+            {
+                if(matrix[i][j]==curr)
+                   check.push_back('s');
+                else
+                    check.push_back('b');
             }
-
-            for (auto& row : matrix) {
-
-                if (row == currRow || row == inverted) {
-                    count++;
-                }
-            }
-
-            maxRows = max(maxRows, count);
+            cout<<check<<endl;
+            mp[check]++;
         }
-
-        return maxRows;
+        
+        int ans=0;
+        for(auto it : mp)
+        {
+            ans=max(ans,it.second);
+        }
+        return ans;
     }
 };
