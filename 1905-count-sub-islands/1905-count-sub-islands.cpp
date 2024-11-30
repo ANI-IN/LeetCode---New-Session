1e65e92ddc21e7f4a2f9ce8d21c63694c1f5da3b
Animesh Kumar <44803072+ANI-IN@users.noreply.github.com>
2024-11-30 23:20:36 +0530

    Time: 151 ms (34.35%), Space: 228.5 MB (9.88%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,17 +1,16 @@
 class Solution {
-    int solve(int i, int j, vector<vector<int>>& grid1, vector<vector<int>>& grid2, vector<vector<int>>& vis)
+    int solve(int i, int j, vector<vector<int>>&grid1, vector<vector<int>>&grid2,vector<vector<int>> &vis)
     {
-        queue<pair<int,int>> q;
+        queue<pair<int,int>>q;
+        bool ans=true;
         int n=grid1.size();
-        int m=grid1[0].size();
+        int m=grid2[0].size();
         
-        int delrow[]={-1,0,+1,0};
-        int delcol[]={0,+1,0,-1};
+        int dx[4]={0,+1,0,-1};
+        int dy[4]={+1,0,-1,0};
         
-        vis[i][j]=1;
         q.push({i,j});
         
-        bool island=true;
         
         while(!q.empty())
         {
@@ -20,44 +19,43 @@ class Solution {
             q.pop();
             
             if(grid1[r][c]!=1)
-            island=false;
+                ans=false;
             
-            for(int k=0;k<4;k++)
+            for(int i=0;i<4;i++)
             {
-                int nr=r+delrow[k];
-                int nc=c+delcol[k];
+                int nr=r+dx[i];
+                int nc=c+dy[i];
                 
                 if(nr>=0 && nr<n && nc>=0 && nc<m && grid2[nr][nc]==1 && !vis[nr][nc])
                 {
-                        vis[nr][nc]=1;
-                        q.push({nr,nc});
+                    vis[nr][nc]=1;
+                    q.push({nr,nc});
                 }
             }
         }
         
-        if(island)
+        if(ans)
             return 1;
         else
             return 0;
     }
 public:
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-        int n=grid1.size();
+        int n=grid2.size();
         int m=grid2[0].size();
-        int c=0;
         vector<vector<int>> vis(n,vector<int>(m,0));
-        
+        int ans=0;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(grid2[i][j]==1 && !vis[i][j])
                 {
-                    int ans=solve(i,j,grid1,grid2,vis);
-                    c=c+ans;
+                    int c = solve(i,j,grid1,grid2,vis);
+                    ans+=c;
                 }
             }
         }
-        return c;
+        return ans;
     }
 };
