afdb95ec452832db21006ec5fa877ea1d1e44fd0
Animesh Kumar <44803072+ANI-IN@users.noreply.github.com>
2024-12-19 19:55:45 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (35.22%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
@@ -2,36 +2,16 @@ class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
         int n=arr.size();
-        vector<int> prefix=arr;
-        vector<int> suffix=arr;
-        
-        prefix[0]=arr[0];
-        suffix[n-1]=arr[n-1];
-        
-        for(int i=1;i<n;i++)
-        {
-            prefix[i] = max(prefix[i-1],arr[i]);
-             
-        }
-        
-        for(int i=n-2;i>=0;i--)
-        {
-            suffix[i] = min(suffix[i+1],suffix[i]);
-        }
         int ans=0;
-        
-        for(int i=0;i<n;i++)
-            cout<<suffix[i]<<" ";
-        cout<<endl;
-        
-     
+        int pre=0,suff=0;
         for(int i=0;i<n;i++)
         {
-            if(i==0 || suffix[i] > prefix[i-1])
+            pre+=arr[i];
+            suff+=i;
+            
+            if(pre==suff)
                 ans++;
         }
-         
-        
         return ans;
     }
 };
