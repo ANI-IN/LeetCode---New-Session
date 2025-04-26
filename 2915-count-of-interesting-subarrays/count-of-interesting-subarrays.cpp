f8204cab35692f06f74a9263fbb30c900956e4a9
Animesh Kumar <44803072+ANI-IN@users.noreply.github.com>
2025-04-26 15:11:51 +0530

    Time: 49 ms (75.39%) | Memory: 126.9 MB (13.09%) - LeetSync

--- 2915-count-of-interesting-subarrays/count-of-interesting-subarrays.cpp
+++ 2915-count-of-interesting-subarrays/count-of-interesting-subarrays.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     long long countInterestingSubarrays(vector<int>& nums, int m, int k) {
-        int n = nums.size();
-
-        unordered_map<int, long long> mp; //remainder -> count
-        long long sum = 0; //sum will be equal to cnt of those elements % m == k
-
-        long long result = 0;
-
-        mp[0] = 1;
-        for(int i = 0; i < n; i++) {
-            if(nums[i] % m == k)
-                sum += 1;
-
-            int r1 = sum%m;
-
-            int r2 = (r1 - k + m) % m;
-
-            result += mp[r2];
-            mp[r1]++;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]%m==k)
+            nums[i]=1;
+            else
+            nums[i]=0;
         }
 
-        return result;
+        unordered_map<int,long long> mp;
+        long long sum=0;
+        long long ans=0;
+        mp[0]=1;
+        for(int i=0;i<n;i++)
+        {
+            sum +=nums[i];
+            int r1 = sum%m;
+            int r2 = (r1 - k + m )%m;
 
+            ans+=mp[r2];
+            mp[r1]++;
+        }
+        return ans;
     }
 };
-
